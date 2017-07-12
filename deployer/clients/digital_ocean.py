dd2b467b7bbfbbcdc6ae060d3786952c7b0f1e3a
Ben Muschol <benmuschol@gmail.com>
2017-07-12 19:01:19 -0400

    Add interactions for deleting domain records

--- deployer/clients/digital_ocean.py
+++ deployer/clients/digital_ocean.py
@@ -9,6 +9,15 @@ class NoDropletError(Exception):
         message = "No droplet found with name '%s'" % name
         super(NoDropletError, self).__init__(message, args)
 
+class NoRecordError(Exception):
+    def __init__(self, name, *args):
+        message = "No domain record found with name '%s'" % name
+        super(NoRecordError, self).__init__(message, args)
+
+class NoRecordError(Exception):
+    def __init__(self, name, *args):
+        message = "No domain record found with name '%s'" % name
+        super(NoRecordError, self).__init__(message, args)
 
 def get_droplet(droplet_name):
     manager = digitalocean.Manager(token=token)
@@ -18,8 +27,19 @@ def get_droplet(droplet_name):
             return droplet
     raise NoDropletError(droplet_name)
 
-def create_domain_record(name, ip):
+def create_domain_record(name, ip, domain='nu-tab.com'):
     record_type = 'A'
-    domain = digitalocean.Domain(token=token, name='nu-tab.com')
+    domain = digitalocean.Domain(token=token, name=domain)
 
     return domain.create_new_domain_record(type='A', name=name, data=ip)
+
+def destroy_domain_record(name, domain='nu-tab.com'):
+    return get_domain_record(name, domain).destroy()
+
+def get_domain_record(name, domain='nu-tab.com'):
+    domain = digitalocean.Domain.get_object(token, domain)
+    records = domain.get_records(params={ 'name': name })
+    for record in records:
+        if record.name == name:
+            return record
+    raise NoRecordError(name)
