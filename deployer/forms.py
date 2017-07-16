c5d7e016e229c5c1b35cfa6ccb5a7fdee7062d27
Ben Muschol <benmuschol@gmail.com>
2017-07-16 14:08:41 -0400

    Implement validation of unique tournament names (#17)

--- deployer/forms.py
+++ deployer/forms.py
@@ -4,13 +4,19 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
 
+from deployer.models import Droplet
+
 def validate_name(form, field):
     pattern = re.compile('^[\w\d\-]+$')
     if not pattern.match(field.data):
         raise ValidationError('Name contains invalid characters')
 
+def validate_unique_name(form, field):
+    if Droplet.query.filter_by(name=field.data.lower()).count() > 0:
+        raise ValidationError('A tournament with that name already exists')
+
 class TournamentForm(FlaskForm):
-    name = StringField('Tournament Name', [DataRequired(), validate_name])
+    name = StringField('Tournament Name', [DataRequired(), validate_name, validate_unique_name])
     email = StringField('Email Address', [Email()])
     password = PasswordField('Password', [
         DataRequired(),
