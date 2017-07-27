a2809c2ff1adbf4d2e7ffee8a6b5469543e9be9f
Ben Muschol <benmuschol@gmail.com>
2017-07-27 16:20:27 -0400

    Add sentry for error monitoring (#21)

--- deployer/__init__.py
+++ deployer/__init__.py
@@ -3,12 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_hookserver import Hooks
 from flask_mail import Mail
 from flask_migrate import Migrate
+from raven.contrib.flask import Sentry
 
 from config.base import BaseConfig
 
 app = Flask('deployer')
 app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
+sentry = Sentry(app, dsn='https://d902814c71b2463a8c4ac7eef7c30481:8a09649a0cf844df9c77f2fcbb09a1f2@sentry.io/196701')
 
 from deployer.models import *
 
