4e30fe04f5aed687afea4d1926b20ce28a6fb78e
Ben Muschol <benmuschol@gmail.com>
2017-09-08 17:46:51 -0400

    Get it working

--- deployer/tasks.py
+++ deployer/tasks.py
@@ -81,7 +81,7 @@ def deploy_droplet(droplet, password, size):
             raise ServerNotReadyError()
 
         droplet.set_status('Installing mit-tab on server')
-        command = './bin/setup_droplet {} {} {} {}'.format(
+        command = './bin/setup_droplet {} {} {} {} {}'.format(
                 droplet.ip_address,
                 droplet.clone_url,
                 droplet.branch,
