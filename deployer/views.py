7ad677b10389a4d859322278588d22c364ce5ba3
Ben Muschol <benmuschol@gmail.com>
2019-01-17 22:16:03 -0500

    Initial version without simple checkout.js integration

--- deployer/views.py
+++ deployer/views.py
@@ -1,12 +1,13 @@
 import os
 
-from flask import render_template, redirect, jsonify, request
+from flask import render_template, redirect, jsonify, request, flash
 
 from config.repo_options import options
 from deployer import hooks
 from deployer.models import *
 from deployer.tasks import *
 from deployer.forms import TournamentForm
+from deployer.clients import stripe
 
 
 @app.route('/', methods=['GET'])
@@ -18,27 +19,34 @@ def index():
 def new_tournament():
     form = TournamentForm()
     if form.validate_on_submit():
-        repo_data = options[form.repo_options.data]
-        tournament = Tournament(form.name.data,
-                                repo_data['clone_url'],
-                                repo_data['branch'])
+        if stripe.charge(request.form['stripeEmail'], request.form['stripeToken']):
+            repo_data = options[form.repo_options.data]
+            tournament = Tournament(form.name.data,
+                                    repo_data['clone_url'],
+                                    repo_data['branch'])
 
-        db.session.add(tournament)
-        db.session.commit()
+            db.session.add(tournament)
+            db.session.commit()
 
-        tournament.set_status('Initializing')
-        deploy_tournament.delay(tournament.id,
-                                form.password.data,
-                                form.email.data)
+            tournament.set_status('Initializing')
+            deploy_tournament.delay(tournament.id,
+                                    form.password.data,
+                                    form.email.data)
 
-        if form.add_test.data:
-            deploy_test.delay(tournament.name,
-                              tournament.clone_url,
-                              tournament.branch)
-
-        return redirect('/tournaments/%s' % tournament.name)
+            if form.add_test.data:
+                deploy_test.delay(tournament.name,
+                                tournament.clone_url,
+                                tournament.branch)
+            return redirect('/tournaments/%s' % tournament.name)
+        else:
+            flash("""
+                  Error processing payment. Contact Ben (email in footer) if
+                  the problem persists
+                  """)
 
     return render_template('new.html',
+                           stripe_cost=stripe.COST_IN_CENTS,
+                           stripe_key=stripe.get_publishable_key(),
                            title='Create a Tournament',
                            form=form)
 
