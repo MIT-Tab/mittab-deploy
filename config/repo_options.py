7ecd3d708c3b9b29e52ec350a60b236a85855636
Ben Muschol <benmuschol@gmail.com>
2019-10-12 12:24:14 -0700

    Remove motions repo option. Not up-to-date
    TODO: Add a github action to automatically update the branch

--- config/repo_options.py
+++ config/repo_options.py
@@ -4,9 +4,4 @@ options = {
         "branch": "master",
         "name": "Default"
     },
-    "motions": {
-        "clone_url": "https://github.com/jolynch/mit-tab.git",
-        "branch": "motions",
-        "name": "Motions (pairing alg will treat govs/opps the same)"
-    },
 }
