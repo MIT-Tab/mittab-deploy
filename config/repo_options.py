e753b1025790eff2bb0906f3d8439dbefcf8dcf8
Ben Muschol <ben.muschol@airbnb.com>
2019-10-15 11:26:24 -0700

    Add RJ's outs as an option

--- config/repo_options.py
+++ config/repo_options.py
@@ -4,4 +4,9 @@ options = {
         "branch": "master",
         "name": "Default"
     },
+    "default": {
+        "clone_url": "https://github.com/roddajohn/mit-tab.git",
+        "branch": "outrounds",
+        "name": "Outrounds - BETA, only use with explicit approval"
+    },
 }
