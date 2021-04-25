f811278b4b75ebde25325441be4d767fa7db7cb3
Ben Muschol <ben.muschol@airbnb.com>
2021-04-25 10:54:04 -0400

    WIP

--- deployer/clients/digital_ocean.py
+++ deployer/clients/digital_ocean.py
@@ -3,6 +3,7 @@ from time import time
 
 import boto3
 import digitalocean
+import requests
 
 __access_key = os.environ['DIGITALOCEAN_ACCESS_KEY_ID']
 __secret_key = os.environ['DIGITALOCEAN_ACCESS_KEY_SECRET']
@@ -71,6 +72,92 @@ def __get_image_slug():
     return sorted(filter(lambda i: i.slug and ('docker' in i.slug), images), key=lambda i: i.created_at)[0].slug
     """
     return 'docker-20-04'
+
+
+############################
+# Apps
+############################
+
+def create_app(name, tab_password, database):
+    """
+    Create the main App. DB must be created first.
+    """
+    resp = requests.post("https://api.digitalocean.com/v2/apps",
+            json=__build_app_spec(name, tab_password, database),
+            headers={"Authorization": f"Bearer {__token}"})
+    resp.raise_for_status()
+
+
+def __build_app_spec(name, tab_password, database):
+    """
+    Build the appspec object for digitalocean. Ideally we could use a yaml file on the
+    branch of the deployment, but it gets a bit tricky with databases and secrets, which
+    cannot be easily managed through that file.
+
+    Reference: https://docs.digitalocean.com/products/app-platform/references/app-specification-reference/
+    """
+    def env_var(key, value, is_secret=False):
+        return {
+            "key": key,
+            "value": value,
+            "type": "SECRET" if is_secret else "GENERAL",
+        }
+
+    github_config = {
+        "repo": "MIT-Tab/mit-tab",
+        "branch": "do-apps", # TODO: customize
+        "deploy_on_push": False,
+    }
+
+    return {
+        "name": f"mittab-{name}",
+        "services": [{
+            "name": "web",
+            "instance_count": 1,
+            "instance_size_slug": "profesional-xs",
+            "dockerfile_path": "Dockerfile",
+            "http_port": 8000,
+            "github": github_config,
+            "routes": [{ "path": "/" }],
+        }],
+        "static_sites": [{
+            "name": "static",
+            "output_dir": "/var/www/tab/assets",
+            "dockerfile_path": "Dockerfile",
+            "github": github_config,
+            "routes": [{ "path": "/static" }]
+        }],
+        "envs": [
+            env_var("TAB_PASSWORD", tab_password, True),
+            env_var("MYSQL_DATABASE", "${mysql.DATABASE}"),
+            env_var("MYSQL_PASSWORD", "${mysql.PASSWORD}"),
+            env_var("MYSQL_USER", "${mysql.USERNAME}"),
+            env_var("MYSQL_HOST", "${mysql.HOSTNAME}"),
+            env_var("MYSQL_PORT", "${mysql.PORT}"),
+            env_var("BACKUP_STORAGE", "S3"),
+            env_var("BACKUP_BUCKET", "mittab-backups"),
+            env_var("BACKUP_PREFIX", f"backups/{name}/{int(time())}"),
+            env_var("BACKUP_S3_ENDPOINT", "https://nyc3.digitaloceanspaces.com"),
+            env_var("AWS_ACCESS_KEY_ID", __access_key, True),
+            env_var("AWS_SECRET_ACCESS_KEY_ID", __secret_key, True),
+            env_var("SENTRY_DSN", os.environ.get("MITTAB_SENTRY_DSN"), True),
+            env_var("TOURNAMENT_NAME", name),
+            env_var("DISCORD_BOT_TOKEN", os.environ.get("DISCORD_BOT_TOKEN", True)),
+        ],
+        "databases": [{
+            "name": "mysql",
+            "production": Truem
+            "engine": "MySQL",
+            "db_name": "foo",      #TODO
+            "db_user": "foo",      #TODO
+            "cluster_name": "baz", #TODO
+        }],
+        "domains": [{
+            "name": f"{name}.nu-tab.com",
+            "type": "PRIMARY",
+            "wildcard": False,
+        }]
+    }
 
 
 ############################
